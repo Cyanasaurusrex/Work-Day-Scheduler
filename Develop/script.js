fe1972c4f0eae7a99d280f8f6a09c080a92209d4
Nathan Moon <moonman700@gmail.com>
2023-02-26 16:50:09 -0500

    removed readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,4 @@
 var saveButton = $('.saveBtn')
-
 var hour9Text = $('#hour9Text')
 var hour10Text = $('#hour10Text')
 var hour11Text = $('#hour11Text')
@@ -26,23 +25,46 @@ var currentHour = now.format('hA')
 var currentDayEl = $('#currentDay');
 
 // Set the text content of the element to the current hour
-currentDayEl.text(currentHour);
-
+currentDayEl.text(currentDay);
+// function to determine current time and dynamically change classes of hours to show past present and future events
 function changeClass() {
+
   let hourListfull = [hour9, hour10, hour11, hour12, hour1, hour2, hour3, hour4, hour5]
   let hourlist = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+  console.log(now.format('H'))
+
+  // check to see if current hour is outside of office hours
+  // if current hour is after midnight but before 9am, sets all classes to "future"
+  if (now.format('H') < 9) {
+    for (i = 0; i < hourlist.length; i++) {
+      hourListfull[i].removeClass("present")
+      hourListfull[i].removeClass("past")
+      hourListfull[i].addClass("future")
+    }
+  }
+  // if the current hour is after 5pm but before midnight, sets all classes to "past"
+  if (now.format('H') > 17) {
+    for (i = 0; i < hourlist.length; i++) {
+      hourListfull[i].removeClass("present")
+      hourListfull[i].removeClass("future")
+      hourListfull[i].addClass("past")
+    }   
+  }
+  // loops through the hours to find the current hour and set its class to "present"
   for (i = 0; i < hourlist.length; i++) {
     if (now.format('h') == hourlist[i]) {
       hourListfull[i].removeClass("past")
       hourListfull[i].removeClass("future")      
       hourListfull[i].addClass("present")
       console.log(hourlist[i] + "PM set to present")
+      // for each preceding hour, set the class to "past"
       for (j = 0; j < i; j ++){
         hourListfull[j].removeClass("present")
         hourListfull[j].removeClass("future")
         hourListfull[j].addClass("past")
         console.log(hourlist[j] + " PM set to past")
       }
+      // for the remaining hours, set the class to "future"
       for (k = (i + 1); k < hourlist.length; k++) {
         hourListfull[k].removeClass("present")
         hourListfull[k].removeClass("past")
@@ -53,9 +75,8 @@ function changeClass() {
   }
 }
 
-$(document).ready(function() {
-  changeClass()  
-
+$(document).ready(function() {  
+  changeClass()
   hour9Text.text(localStorage.getItem('hour9'))
   hour10Text.text(localStorage.getItem('hour10'))
   hour11Text.text(localStorage.getItem('hour11'))
@@ -65,14 +86,11 @@ $(document).ready(function() {
   hour3Text.text(localStorage.getItem('hour3'))
   hour4Text.text(localStorage.getItem('hour4'))
   hour5Text.text(localStorage.getItem('hour5'))
-  
-
   saveButton.on('click', function() {    
     var parentId = $(this).parent().attr('id');
     var userImput = $(this).siblings('.description').val();
     localStorage.setItem(parentId, userImput)        
-  })
-  
+  })  
 });
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
