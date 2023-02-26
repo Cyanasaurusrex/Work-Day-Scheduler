274f68448be5b8debb570a3f19708418883753d3
Nathan Moon <moonman700@gmail.com>
2023-02-26 17:19:02 -0500

    auto-refreshes the page and checks if present time is after office hours

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// variable for save buttons used in event listener
 var saveButton = $('.saveBtn')
+// variables for the text content of each textarea
 var hour9Text = $('#hour9Text')
 var hour10Text = $('#hour10Text')
 var hour11Text = $('#hour11Text')
@@ -8,6 +10,7 @@ var hour2Text = $('#hour2Text')
 var hour3Text = $('#hour3Text')
 var hour4Text = $('#hour4Text')
 var hour5Text = $('#hour5Text')
+// variables for the <div> containing each row
 var hour9 = $("#hour9")
 var hour10 = $("#hour10")
 var hour11 = $("#hour11")
@@ -29,9 +32,9 @@ currentDayEl.text(currentDay);
 // function to determine current time and dynamically change classes of hours to show past present and future events
 function changeClass() {
 
+  //sets lists of current hours for logical operations below
   let hourListfull = [hour9, hour10, hour11, hour12, hour1, hour2, hour3, hour4, hour5]
   let hourlist = [9, 10, 11, 12, 1, 2, 3, 4, 5]
-  console.log(now.format('H'))
 
   // check to see if current hour is outside of office hours
   // if current hour is after midnight but before 9am, sets all classes to "future"
@@ -56,27 +59,32 @@ function changeClass() {
       hourListfull[i].removeClass("past")
       hourListfull[i].removeClass("future")      
       hourListfull[i].addClass("present")
-      console.log(hourlist[i] + "PM set to present")
       // for each preceding hour, set the class to "past"
       for (j = 0; j < i; j ++){
         hourListfull[j].removeClass("present")
         hourListfull[j].removeClass("future")
         hourListfull[j].addClass("past")
-        console.log(hourlist[j] + " PM set to past")
       }
       // for the remaining hours, set the class to "future"
       for (k = (i + 1); k < hourlist.length; k++) {
         hourListfull[k].removeClass("present")
         hourListfull[k].removeClass("past")
-        hourListfull[k].addClass("future")
-        console.log(hourlist[k] + "PM set to future")       
+        hourListfull[k].addClass("future")      
       }
     }
   }
 }
 
-$(document).ready(function() {  
-  changeClass()
+// waits for page to finish loading before executing the formatting and local storage retrieval / event listener
+$(document).ready(function() {
+  // calls change class function when page loads and then calls it again every second after
+  changeClass()  
+  setInterval(() => {
+    now = dayjs()
+    changeClass()    
+  }, 1000);
+
+  //displays previously stored text on the page 
   hour9Text.text(localStorage.getItem('hour9'))
   hour10Text.text(localStorage.getItem('hour10'))
   hour11Text.text(localStorage.getItem('hour11'))
@@ -86,33 +94,12 @@ $(document).ready(function() {
   hour3Text.text(localStorage.getItem('hour3'))
   hour4Text.text(localStorage.getItem('hour4'))
   hour5Text.text(localStorage.getItem('hour5'))
+
+  // event listener for user clicks on save button
+  // stores any text in the textarea to local storage
   saveButton.on('click', function() {    
     var parentId = $(this).parent().attr('id');
     var userImput = $(this).siblings('.description').val();
     localStorage.setItem(parentId, userImput)        
   })  
 });
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
